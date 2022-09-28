4a1a234b863c70da879444ea5de3a0068f708b7f
Alexandr <alex.k.43rus@gmail.com>
2022-09-28 21:44:46 +0300

    Законченный первый урок

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -2,26 +2,36 @@
 Console.Write("Введите ваше имя:");
 string username = Console.ReadLine();
 
+//Запоминаем конкретное имя
+
 if (username.ToLower() == "саша")
 {
     Console.WriteLine("Ура, это же САША");
 }
 Console.Write("Привет, ");
-Console.WriteLine("username");
+Console.WriteLine(username);
+
+
+//Сложение и деление случайных чисел
+
 
 int numberA = new Random().Next(2, 99);
-Console.WriteLine(numberA);
 int numberB = new Random().Next(2, 99);
+Console.Write(numberA);
+Console.Write("+");
 Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.WriteLine(numberA + numberB);
+Console.Write("Результат сложения: ");
 Console.WriteLine(result);
 double numberC = new Random().Next(2, 99);
-Console.WriteLine(numberC);
 double numberD = new Random().Next(2, 99);
+Console.Write(numberC);
 Console.WriteLine(numberD);
-Console.WriteLine("Результат деления: ", numberC/numberD);
+double resultdel = numberC / numberD;
+Console.Write("Результат деления: ");
+Console.WriteLine(resultdel);
 
+//Задача про гири
 
 int a = 1;
 int b = 2;
@@ -36,17 +46,19 @@ if (c > max) max = c;
 if (d > max) max = d;
 if (e > max) max = e;
 
-Console.Write(max);
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
+
 
 //Задача про точки
 
-Console.Clear();
-Console.SetCursorPosition(10, 4);
-Console.WriteLine("+");
+//Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
 
-int xa = 1, ya = 1, 
-    xb = 1, yb = 30;
-    xc = 30, yc = 30;
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -57,15 +69,28 @@ Console.WriteLine("+");
 
 int x = xa, y = ya;
 int count = 0;
-while(count < 10)
+while(count < 10000)
 {
-    int what = new Random().Next(0, 3);
+    int what = new Random().Next(0, 3); //Интервал 0,1,2
     if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
 
     if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
 
     if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1; //count++
 }
-Console.SetCursorPosition(x, y);
-Console.WriteLine("+");
-count = count + 1;
+
