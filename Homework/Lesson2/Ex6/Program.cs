8093dca9b3d7848245eeb3963a687118bac6a8a7
Alexandr <alex.k.43rus@gmail.com>
2022-10-03 13:59:22 +0300

    Готово

--- Homework/Lesson2/Ex6/Program.cs
+++ Homework/Lesson2/Ex6/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x%2 == 0)
+    Console.Write("Это четное число ");
+
+else if (x%2 == 1)
+    Console.Write("Это число не является четным ");
+
+    
