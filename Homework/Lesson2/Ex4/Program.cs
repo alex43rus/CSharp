1a1f3d357b25c5735cafcfd25c7d6d51d82ac2c3
Alexandr <alex.k.43rus@gmail.com>
2022-10-03 13:33:30 +0300

    Готово

--- Homework/Lesson2/Ex4/Program.cs
+++ Homework/Lesson2/Ex4/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: "); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите второе число: "); 
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: "); 
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (max < b)
+    max = b;
+else if (max < c)
+    max = c;
+
+Console.WriteLine(max);
