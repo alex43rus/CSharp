12653cb8e6b2818afc70e13730414b8a28f6ef58
Alexandr <alex.k.43rus@gmail.com>
2022-10-05 16:06:48 +0300

    Готово

--- Homework/Lesson2/Ex10/Program.cs
+++ Homework/Lesson2/Ex10/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n > 100)
+{
+    n = n / 10;
+}
+
+Console.Write($"Второе число {n % 10}");
