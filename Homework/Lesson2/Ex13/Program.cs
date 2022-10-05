ac443c34ed3b50ec25af7b536b4f3f7c062688fa
Alexandr <alex.k.43rus@gmail.com>
2022-10-05 16:24:19 +0300

    Готово

--- Homework/Lesson2/Ex13/Program.cs
+++ Homework/Lesson2/Ex13/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет. (Можно воспользоваться нумерацией строк, но необязательно)
+
+Console.Write("Введите число: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+while (n > 1000)
+{
+    
+    n = n / 10;
+    
+}
+
+if (n < 100)
+{
+    Console.Write($"В вашем числе {n} нет третьей цифры");
+}
+
+else
+{
+    Console.Write($"Третье число {n % 10}");
+}
+
