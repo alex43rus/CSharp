170d9d630044e41ec2f33586e7bcd234bb02ea17
Alexandr <alex.k.43rus@gmail.com>
2022-10-11 03:40:37 +0300

    Done

--- Homework/Lesson2/Ex19/Program.cs
+++ Homework/Lesson2/Ex19/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+while (chislo < 9999 || chislo > 99999)
+{
+    Console.Write("Неправильно, введите пятизначное число: ");
+    chislo = Convert.ToInt32(Console.ReadLine());
+} 
+
+int temp = chislo;
+int [] revers = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    revers [i] = temp % 10;
+    temp /= 10;
+}  
+
+int result = 0;
+for (int i = 0; i < 5; i++)
+{
+    int index = 5 - i - 1;
+    result += ((int)Math.Pow(10, index)) * revers[i];
+}
+Console.WriteLine("[" + string.Join(", ", revers) + "]");
+Console.WriteLine(result);
+Console.WriteLine(chislo);
+if (chislo == result)
+    Console.WriteLine("Данное число является палиндромом");
+else Console.WriteLine("Данное число не является палиндромом");
