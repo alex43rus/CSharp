a2f3172de06b3f3d1af6d8c6fb982e49c5b17971
Alexandr <alex.k.43rus@gmail.com>
2022-10-11 04:00:03 +0300

    Done

--- Homework/Lesson2/Ex23/Program.cs
+++ Homework/Lesson2/Ex23/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    
+    Console.Write($"{i * i * i} ");
+
+}
+
