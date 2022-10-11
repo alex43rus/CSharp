487944841820ac1e68f5a5a37884ea629e94c58a
Alexandr <alex.k.43rus@gmail.com>
2022-10-11 03:54:47 +0300

    Готово

--- Homework/Lesson2/Ex21/Program.cs
+++ Homework/Lesson2/Ex21/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21
+// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+Console.Write("Введите координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)), 3));
