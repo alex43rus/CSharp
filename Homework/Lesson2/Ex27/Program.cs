e515fe93f2c0ad5235ce0c7ffe8eb4c4e2f5a258
Alexandr <alex.k.43rus@gmail.com>
2022-10-12 17:08:07 +0300

    1

--- Homework/Lesson2/Ex27/Program.cs
+++ Homework/Lesson2/Ex27/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//n = Math.Abs(n);
+int rez = 0;
+
+while (n > 0)
+{
+    rez = rez + n % 10;
+}
+
+
+Console.WriteLine(rez);
+
