bc155b7dd6770113f68191b7ff6b238a70f1b57b
Alexandr <alex.k.43rus@gmail.com>
2022-10-12 16:10:00 +0300

    Готово

---
+++ Homework/Lesson2/Ex25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int rez = 1;
+
+for (int i = 0; i < b; i++)
+{
+    rez = rez * a;
+}
+Console.WriteLine(rez);
--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿//Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// double n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine((1 + n) / 2 * n);
+
+
+//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+// while (n > 0)
+// {
+//     n = n / 10;
+//     result++; // result = result + 1
+// }
+// Console.WriteLine(result);
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int[] array = new int[8];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//    array[i] = new Random().Next(0, 2);
+
+// }
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
