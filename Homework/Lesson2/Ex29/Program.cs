b1f3e182b4565ea1bde27d0ca0821bf07934ed58
Alexandr <alex.k.43rus@gmail.com>
2022-10-13 17:11:15 +0300

    Done

---
+++ Homework/Lesson2/Ex29/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+   array[i] = new Random().Next();
+
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
