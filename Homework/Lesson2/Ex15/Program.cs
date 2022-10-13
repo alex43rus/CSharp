10bd5c5e635f0c98c35580748c2177445ce2cb5a
Alexandr <alex.k.43rus@gmail.com>
2022-10-13 16:43:13 +0300

    Done

--- Homework/Lesson2/Ex15/Program.cs
+++ Homework/Lesson2/Ex15/Program.cs
@@ -1,35 +1,35 @@
 ﻿//Напишите программу, которая принимает на вход цифру, 
 //обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.Write("Введите номер дня недели: ");
+// Console.Write("Введите номер дня недели: ");
 
-while (days < 1 || days > 7) 
-{
-    Console.Write("Неверно, введите номер дня недели снова: ");
-    int days = Convert.ToInt32(Console.ReadLine());
-}
+// while (days < 1 || days > 7) 
+// {
+//     Console.Write("Неверно, введите номер дня недели снова: ");
+//     int days = Convert.ToInt32(Console.ReadLine());
+// }
 
-if (days == 1) 
-    Console.Write("Понедельник ");
+// if (days == 1) 
+//     Console.Write("Понедельник ");
 
-else if (days == 2) 
-    Console.Write("Вторник ");
+// else if (days == 2) 
+//     Console.Write("Вторник ");
 
-else if (days == 3) 
-    Console.Write("Среда ");
+// else if (days == 3) 
+//     Console.Write("Среда ");
 
-else if (days == 4) 
-    Console.Write("Четверг ");
+// else if (days == 4) 
+//     Console.Write("Четверг ");
 
-else if (days == 5) 
-    Console.Write("Пятница ");
+// else if (days == 5) 
+//     Console.Write("Пятница ");
 
-else if (days == 6) 
-    Console.Write("Суббота ");
+// else if (days == 6) 
+//     Console.Write("Суббота ");
 
-else if (days == 7) 
-    Console.Write("Воскресенье ");
+// else if (days == 7) 
+//     Console.Write("Воскресенье ");
 
 
-if (days == 6 || days == 7) 
-    Console.Write("Сегодня у вас выходной");
+// if (days == 6 || days == 7) 
+//     Console.Write("Сегодня у вас выходной");
--- Homework/Lesson2/Ex27/Program.cs
+++ Homework/Lesson2/Ex27/Program.cs
@@ -5,10 +5,13 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 //n = Math.Abs(n);
 int rez = 0;
+int temp = 0;
 
 while (n > 0)
 {
-    rez = rez + n % 10;
+    temp = n % 10;
+    rez = rez + temp;
+    n = n / 10;
 }
 
 
