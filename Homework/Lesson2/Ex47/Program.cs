3a7018c69bd3286a9a73f626c8844be446d070fa
Alexandr <alex.k.43rus@gmail.com>
2022-10-21 16:08:11 +0300

    Done. как добавить отрицательные числа?

---
+++ Homework/Lesson2/Ex47/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Double[,] matrix = new Double[n, m];
+InputMatrix(matrix);
---
+++ Seminar7/Program.cs
@@ -0,0 +1,151 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(-10, 11);
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во строк: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[n, m];
+// InputMatrix(matrix);
+
+
+// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = i + j;
+//             Console.Write($"{matrix[i, j]} \t");
+            
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во строк: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[n, m];
+// InputMatrix(matrix);
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса нечётные, и замените эти элементы на их квадраты.
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             matrix[i, j] = new Random().Next(-10, 11);
+//     }
+// }
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             Console.Write($"{matrix[i, j]} \t");
+//     Console.WriteLine();
+//     }
+// }
+// void ReleaseMatrix(int[,] matrix)
+// {
+//     for (int i = 1; i < matrix.GetLength(0); i+=2)
+//     {
+//         for (int j = 1; j < matrix.GetLength(1); j+=2)
+//             matrix[i, j] *= matrix[i, j];
+//     }
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во строк: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[n, m];
+// InputMatrix(matrix);
+// PrintMatrix(matrix);
+// ReleaseMatrix(matrix);
+// PrintMatrix(matrix);
+
+
+// Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             matrix[i, j] = new Random().Next(-10, 11);
+//     }
+// }
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             Console.Write($"{matrix[i, j]} \t");
+//     Console.WriteLine();
+//     }
+// }
+
+// int ReleaseMatrix(int[,] matrix)
+// {
+//     int summa = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j)
+//                 summa += matrix[i, j];
+//         }
+//     }
+//     return summa;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во строк: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[n, m];
+// InputMatrix(matrix);
+// Console.WriteLine("Начальный массив: ");
+// PrintMatrix(matrix);
+// Console.WriteLine($"Cумма главной диагонали равна: {ReleaseMatrix(matrix)}");
+//PrintMatrix(matrix);
+
+
+
