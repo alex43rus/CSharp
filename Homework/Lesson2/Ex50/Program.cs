a21e753fd302226c2afb3fdd807e63e5e553fd36
Alexandr <alex.k.43rus@gmail.com>
2022-10-23 14:28:56 +0300

    Done

---
+++ Homework/Lesson2/Ex50/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+    Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+Console.Write("Введите строку нужного элемента: ");
+int nn = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец нужного элемента: ");
+int mm = Convert.ToInt32(Console.ReadLine());
+
+if ( nn > n || mm > m) 
+    Console.WriteLine("Такого элемента нет");
+
+else Console.Write($"Ваш элемент: {matrix[nn - 1, mm - 1]} \t");
+
