45696a3d140359fee2de28e84b655356acfe0021
Alexandr <alex.k.43rus@gmail.com>
2022-11-11 18:53:52 +0300

    1

--- Homework/Lesson2/Pascal/Program.cs
+++ Homework/Lesson2/Pascal/Program.cs
@@ -1,9 +1,10 @@
 ﻿int factorial(int n)
 {
     int i, x = 1;
-    for (i = 1, i <= n, i++)
+    for (i = 1; i <= n; i++)
+    {
         x *= i;
-
+    } 
     return x;
 }
 
@@ -11,12 +12,13 @@ Console.Write("Введите количество элементов треуг
 int n = Convert.ToInt32(Console.ReadLine());
 int i, c;
 
-for (i = 0, i < n, i++)
+for (i = 0; i < n; i++)
 {
-    for (c = 0, c <= (n - i), c++)
+    for (c = 0; c <= (n - i); c++)
+    {
         Console.Write(" ");
-
-    for (c = 0, c <= i, c++)
+    }
+    for (c = 0; c <= i; c++)
     {
         Console.Write(" ");
         Console.Write(factorial(i) / (factorial(c) * factorial(i - c)));
