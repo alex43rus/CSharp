29105bc177bb444eb9323186bdc1d86938f29f4c
alex43rus <alex.k.43rus@gmail.com>
2022-11-24 21:56:57 +0300

    Закончил написание программы, все работает исправно, пока не решил как убрать лишние запятые на выводе

--- ItogProject/Program.cs
+++ ItogProject/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void OutputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            Console.Write($"{array[i]}, ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0)
+    {
+    Console.Write("Неверно, введите положительное кол-во элементов массива: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    }
+string[] array = new string[n];
+Console.Write("Вводите элементы массива через пробел: ");
+// for (int i = 0; i <= n; i++)
+//     {     
+//         array[i] = Console.ReadLine().Split(' '); // ввод двух чисел в одну строку
+//     }
+array = Console.ReadLine().Split(' ').ToArray();
+
+OutputArray(array);
