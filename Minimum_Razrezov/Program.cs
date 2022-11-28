e14d69784885ec173d750c23cc51ec203f9829c9
alex43rus <alex.k.43rus@gmail.com>
2022-11-28 21:19:38 +0300

    Done

--- Minimum_Razrezov/Program.cs
+++ Minimum_Razrezov/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите количество человек: ");
+int N = Int32.Parse(Console.ReadLine());
+int m = 0;
+for (m = 0; N > 1; m++)
+{
+    if (N % 2 == 1)
+    {
+        N = N - 1;
+        m++;
+    }
+    N = N / 2;
+}
+Console.WriteLine(m);
